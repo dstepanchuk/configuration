1d6bf80d7255c8192dbe651d6f54f28e97df7ff5
Matthew Potter <matthew.potter@changepoint.com>
2015-03-09 13:22:58 -0700

    Cleaning up files

--- changepoint/windows/setup.ps1
+++ changepoint/windows/setup.ps1
@@ -1,10 +1,48 @@
 #-- Disable shutdown tracker
 .\..\disable-windows-shutdown-event-tracker.ps1
 
-#Import-Module Boxstarter.Chocolatey  does not work because we don't have an updated PsModulePath.
 
+#-- Import the boxstarter hook
 Import-Module $env:AppData\Boxstarter\Boxstarter.Chocolatey -DisableNameChecking
-Install-BoxstarterPackage -PackageName $PSScriptRoot\boxstarter-script.ps1
+#-- Start the Chocolatey package install
+
+#-- win config
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+Disable-InternetExplorerESC
+Enable-RemoteDesktop
+Disable-UAC
+Set-TaskbarOptions -Size Small
+
+#-- system
+cinst powershell4
+
+#-- utilities
+#7zip is installed on the base image
+cinst conemu
+cinst curl
+cinst ie11
+cinst sysinternals
+cinst sumatrapdf
+cinst windirstat
+cinst wireshark
+
+#-- dev tools
+cinst gitextensions
+cinst dotpeek
+cinst markdownpad2
+cinst fiddler4
+cinst linqpad4
+cinst ilmerge
+cinst nuget.commandline
+cinst nugetpackageexplorer
+cinst sublimetext3.app
+cinst windbg
+
+
+#-- task bar items
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles\ConEmu\ConEmu64.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
+
 
 
 #-- Install sublime text packages and user settings
