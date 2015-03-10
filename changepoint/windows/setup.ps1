1941b5472ee115e3939ae17fbef5dc60dd2cf44f
Matthew Potter <matthew.potter@changepoint.com>
2015-03-10 12:56:50 -0700

    Adding visual studio extension install support

--- changepoint/windows/setup.ps1
+++ changepoint/windows/setup.ps1
@@ -39,6 +39,34 @@ cinst -y sublimetext3.app
 cinst -y windbg
 
 
+#-- install visual studio extensions
+#
+# Download and reference NuGet packages
+#
+Write-BoxstarterMessage 'Download and reference NuGet packages'
+$tempPackages = Join-Path $env:TEMP ((Get-Date).Ticks)
+NuGet.exe install -x Newtonsoft.Json -OutputDirectory $tempPackages
+Add-Type -Path (Join-Path $tempPackages 'Newtonsoft.Json\lib\net40\Newtonsoft.Json.dll')
+
+#
+# Install Visual Studio extensions
+#
+Write-BoxstarterMessage 'Install Visual Studio extensions'
+$vsextensions =
+    Get-Content (Join-Path $wininitPath 'vs-extensions.json') -Raw |
+    ConvertFrom-Json
+$vsixInstaller = Join-Path $env:VS110COMNTOOLS '..\IDE\VSIXInstaller.exe'
+$downloads = Join-Path $HOME 'Downloads'
+$webclient = New-Object System.Net.WebClient
+$vsextensions.PSObject.Properties | ForEach-Object {
+    $downloadLocation = Join-Path $downloads $_.Value.FileName
+    $webclient.DownloadFile($_.Value.DownloadUrl, $downloadLocation)
+    & $vsixInstaller /q $downloadLocation
+}
+
+
+
+
 #-- task bar items
 Install-ChocolateyPinnedTaskBarItem "$env:programfiles\ConEmu\ConEmu64.exe"
 Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
